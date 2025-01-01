b277fc945ff7ec4596366ad8a1f04f38c72dad18
Ginjiro Kimura <46777667+kmr0831@users.noreply.github.com>
2025-01-01 23:45:30 +0900

    apply 前に bitwarden-cli を実行できるようにしておく

---
+++ .install-password-manager.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -eufo pipefail
+
+if command -v bw &> /dev/null; then
+  echo "bw is already installed"
+else
+  case "$(uname -s)" in
+  Darwin)
+    if ! command -v brew &> /dev/null; then
+      echo "Homebrew is not installed. Installing Homebrew (pre hook)"
+      /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    else
+    brew install bitwarden-cli
+    ;;
+  Linux*)
+    ;;
+  *)
+    echo "unsupported OS"
+    exit 1
+    ;;
+  esac
+fi
