78e1320dce632cb4045f861efa412fabfa1561ed
Ginjiro Kimura <46777667+kmr0831@users.noreply.github.com>
2025-01-02 09:47:31 +0900

    chezmoi 関連の hook script は .chezmiohooks に移動

--- .chezmoihooks/.install-password-manager.sh
+++ .chezmoihooks/.install-password-manager.sh
@@ -11,6 +11,7 @@ else
       echo "Homebrew is not installed. Installing Homebrew (pre hook)"
       /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     else
+    # eval "$(/opt/homebrew/bin/brew shellenv)"
     brew install bitwarden-cli
     ;;
   Linux*)
