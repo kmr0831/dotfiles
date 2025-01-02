ec7c9e4b996951d4b7fd067f63fcef1dc8fb0702
Ginjiro Kimura <46777667+kmr0831@users.noreply.github.com>
2025-01-02 16:15:55 +0900

    fix syntax error

--- .chezmoihooks/.install-password-manager.sh
+++ .chezmoihooks/.install-password-manager.sh
@@ -10,7 +10,7 @@ else
     if ! command -v brew &> /dev/null; then
       echo "Homebrew is not installed. Installing Homebrew (pre hook)"
       /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    else
+    fi
     # eval "$(/opt/homebrew/bin/brew shellenv)"
     brew install bitwarden-cli
     ;;
