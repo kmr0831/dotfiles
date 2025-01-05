8501ad0aba8f1f56b487cf31371ce86e68287f38
Ginjiro Kimura <46777667+kmr0831@users.noreply.github.com>
2025-01-05 16:12:45 +0900

    Linux で使うことも考慮して修正する

--- .install-password-manager.sh
+++ .install-password-manager.sh
@@ -2,24 +2,38 @@
 
 set -eufo pipefail
 
-if command -v bw &> /dev/null; then
-  echo "bw is already installed"
-  exit 0
-fi
+install_homebrew() {
+  command -v brew >/dev/null 2>&1 && return
 
-case "$(uname -s)" in
+  echo "Homebrew is not installed. Installing Homebrew"
+
+  case "$(uname -s)" in
   Darwin)
-    if ! command -v brew &> /dev/null; then
-      echo "Homebrew is not installed. Installing Homebrew (pre hook)"
-      /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    fi
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     # eval "$(/opt/homebrew/bin/brew shellenv)"
     brew install bitwarden-cli
     ;;
   Linux*)
+    sudo apt-get update && \
+      sudo apt-get upgrade && \
+      sudo apt-get install -y build-essential procps curl file git && \
+      sudo apt-get autoremove && \
+      sudo apt-get autoclean
+
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    # eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+    brew install bitwarden-cli
     ;;
   *)
     echo "unsupported OS"
     exit 1
     ;;
-esac
+  esac
+}
+
+main() {
+  command -v bw >/dev/null 2>&1 && exit
+  install_homebrew
+}
+
+main
