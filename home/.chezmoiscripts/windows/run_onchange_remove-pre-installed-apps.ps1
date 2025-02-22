27e4f8a648df5ec7ac02eca43e0d906837eef595
Ginjiro Kimura <46777667+kmr0831@users.noreply.github.com>
2025-02-22 16:13:01 +0900

    不要なアプリを削除する

--- home/.chezmoiscripts/windows/run_onchange_remove-pre-installed-apps.ps1
+++ home/.chezmoiscripts/windows/run_onchange_remove-pre-installed-apps.ps1
@@ -3,3 +3,23 @@
 # 2. Run `chezmoi apply ~/.chezmoiscripts/windows/remove-bloat.ps1`
 
 # Get-AppxPackage | ft name # インストール済のアプリ名を取得
+
+Get-AppxPackage DolbyLaboratories.DolbyAccess | Remove-AppxPackage
+Get-AppxPackage Microsoft.ZuneMusic | Remove-AppxPackage
+Get-AppxPackage Microsoft.MicrosoftSolitaireCollection | Remove-AppxPackage
+Get-AppxPackage Microsoft.XboxGamingOverlay | Remove-AppxPackage
+Get-AppxPackage Microsoft.XboxIdentityProvider | Remove-AppxPackage
+Get-AppxPackage Microsoft.XboxSpeechToTextOverlay | Remove-AppxPackage
+Get-AppxPackage Microsoft.MicrosoftOfficeHub | Remove-AppxPackage
+Get-AppxPackage Microsoft.Xbox.TCUI | Remove-AppxPackage
+Get-AppxPackage Microsoft.WindowsFeedbackHub | Remove-AppxPackage
+Get-AppxPackage Microsoft.GetHelp | Remove-AppxPackage
+Get-AppxPackage *bing* | Remove-AppxPackage
+Get-AppxPackage Microsoft.YourPhone | Remove-AppxPackage
+Get-AppxPackage Microsoft.WindowsAlarms | Remove-AppxPackage
+Get-AppxPackage Clipchamp.Clipchamp | Remove-AppxPackage
+Get-AppxPackage MSTeams | Remove-AppxPackage
+Get-AppxPackage Microsoft.Todos | Remove-AppxPackage
+# Get-AppxPackage Microsoft.WindowsCamera | Remove-AppxPackage
+Get-AppxPackage Microsoft.Windows.Photos | Remove-AppxPackage
+Get-AppxPackage Microsoft.MicrosoftStickyNotes | Remove-AppxPackage
